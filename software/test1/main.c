12130bd1cc75e7ab34d4a5cfe517061a7e5eb268
Gheorghe <gheorghe.pojoga99@gmail.com>
2022-01-17 00:33:04 +0100

    basic template

---
+++ software/test1/main.c
@@ -0,0 +1,11 @@
+/* Copyright 2019 SiFive, Inc */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#include <stdio.h>
+
+int main() {
+	for (int i = 0; i < 3; ++i)
+		printf("hello my world\n");
+	return 0;
+}
+
